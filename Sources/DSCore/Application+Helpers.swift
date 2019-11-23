5d29f8a65297d98b7dec586c0752c258627010de
Maher Santina <maher.santina90@gmail.com>
2019-11-23 19:56:41 +1100

    Added response helpers

---
+++ Sources/DSCore/Application+Helpers.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maher Santina on 11/23/19.
+//
+
+import Vapor
+
+extension Container {
+    func respondToRequest(request: HTTPRequest) throws -> Future<Response> {
+        let responder = try make(Responder.self)
+        let wrappedRequest = Request(http: request, using: self)
+        return try responder.respond(to: wrappedRequest)
+    }
+}
+
+extension Response {
+    func decode<T>(_ entity: T.Type) throws -> T where T: Decodable {
+        let data = http.body.data ?? Data()
+        let responseEntity = try JSONDecoder().decode(T.self, from: data)
+        return responseEntity
+    }
+}
