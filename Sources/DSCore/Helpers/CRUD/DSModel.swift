f0d2ab74b3c2c7862271f69b3c24cce493fb470b
Maher Santina <maher.santina90@gmail.com>
2019-12-02 18:19:51 +1100

    Added default behavior for model migration

--- Sources/DSCore/Helpers/CRUD/DSModel.swift
+++ Sources/DSCore/Helpers/CRUD/DSModel.swift
@@ -11,6 +11,12 @@ import Vapor
 public protocol DSModel: MySQLModel, CRUD, Content, Migration, Parameter, RouteNameable {  }
 
 extension DSModel {
+    public static func revert(on conn: Database.Connection) -> Future<Void> {
+        return conn.simpleQuery("Drop table if exists \(entity)").transform(to: ())
+    }
+}
+
+extension DSModel {
     public static var defaultDatabase: DatabaseIdentifier<MySQLDatabase>? {
         return .mysql
     }
