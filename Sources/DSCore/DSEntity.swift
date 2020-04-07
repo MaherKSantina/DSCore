0b6d4983801a2b1639a0bef76147ad04b3a99bb7
Maher Santina <maher.santina90@gmail.com>
2020-04-07 20:33:09 +1000

    Added view stuff

--- Sources/DSCore/DSEntity.swift
+++ Sources/DSCore/DSEntity.swift
@@ -9,17 +9,121 @@ import Vapor
 import Fluent
 import FluentMySQLDriver
 
+public extension RawRepresentable where RawValue == String {
+    var fieldKey: FieldKey {
+        return .string(rawValue)
+    }
+}
+
+public protocol DSModelCodingKeys: CodingKey, CaseIterable, DSModelField { }
+public protocol DSViewCodingKeys: CodingKey, CaseIterable, DSViewField { }
+
+public protocol DSViewField {
+    var key: String { get }
+}
+
+public protocol DSModelField: DSViewField {
+    var dataType: DatabaseSchema.DataType { get }
+    var constraints: [DatabaseSchema.FieldConstraint] { get }
+}
+
+public protocol DSModelFields {
+    static var modelFields: [DSModelField] { get }
+}
+
+public protocol DSViewFields {
+    static var viewFields: [DSViewField] { get }
+}
+
 public protocol DSEntity: Model, Content {
 
 }
 
-public protocol DSModel: DSEntityRead, DSEntityWrite, DSDatabaseFieldsRepresentable {
+public protocol DSModel: DSEntityRead, DSEntityWrite, DSDatabaseFieldsRepresentable, DSModelFields {
 
 }
 
-public protocol DSView: DSEntityRead, DSDatabaseViewQuery {
+public protocol DSView: DSEntityRead, DSDatabaseViewQuery, DSViewFields {
 
 }
+
+public enum DSEntityJoin {
+    case left
+    case right
+    case inner
+
+    var query: String {
+        switch self {
+        case .left:
+            return " LEFT JOIN "
+        case .right:
+            return " RIGHT JOIN "
+        case .inner:
+            return " JOIN "
+        }
+    }
+}
+
+public struct ViewInformation {
+    public var schema: String
+    public var fields: [DSViewField]
+}
+
+public struct Join {
+    public var foreignEntity: String
+    public var entityKey: String
+    public var foreignEntityKey: String
+    public var joinType: DSEntityJoin
+
+    public init(foreignEntity: String, entityKey: String, foreignEntityKey: String, joinType: DSEntityJoin) {
+        self.foreignEntity = foreignEntity
+        self.entityKey = entityKey
+        self.foreignEntityKey = foreignEntityKey
+        self.joinType = joinType
+    }
+}
+
+public protocol DSJoinsRepresentable {
+    static var mainEntity: ViewInformation { get }
+    static var entities: [ViewInformation] { get }
+    static var joins: [Join] { get }
+}
+
+public extension DSView where Self: DSJoinsRepresentable {
+    static var viewQuery: String {
+        let select = ([mainEntity] + entities).map { (entity) -> [(String, DSViewField)] in
+            return entity.fields.map{ (entity.schema, $0) }
+        }
+        .flatMap{ $0 }
+        .map{ "`\($0.0)`.`\($0.1.key)` AS `\($0.0)_\($0.1.key)`" }
+        .joined(separator: ", ")
+
+        let joinClause = joins.map{ "\($0.joinType.query) `\($0.foreignEntity)` ON `\($0.foreignEntity)`.`\($0.foreignEntityKey)` = `\(mainEntity.schema)`.`\($0.entityKey)` " }.joined(separator: " ")
+
+        let final = """
+        SELECT
+        `\(mainEntity.schema)`.`\(mainEntity.fields.first?.key ?? "id")` AS `id`,
+        \(select)
+        FROM
+        `\(mainEntity.schema)`
+        \(joinClause)
+        """
+        return final
+    }
+}
+
+public extension DSModel {
+    static var viewInformation: ViewInformation {
+        return ViewInformation(schema: schema, fields: modelFields.map{ $0 })
+    }
+}
+
+public extension DSView {
+    static var viewInformation: ViewInformation {
+        return ViewInformation(schema: schema, fields: viewFields)
+    }
+}
+
 
 public protocol DSEntityRead: DSEntity {
     static var queryField: String? { get }
@@ -75,6 +179,16 @@ public protocol DSDatabaseFieldsRepresentable {
     static func setupFields(schemaBuilder: SchemaBuilder) -> SchemaBuilder
 }
 
+public extension DSDatabaseFieldsRepresentable where Self: DSModelFields {
+    static func setupFields(schemaBuilder: SchemaBuilder) -> SchemaBuilder {
+        var newSchemaBuilder = schemaBuilder
+        modelFields.forEach { (modelField) in
+            newSchemaBuilder = newSchemaBuilder.field(.definition(name: .key(FieldKey.string(modelField.key)), dataType: modelField.dataType, constraints: modelField.constraints))
+        }
+        return newSchemaBuilder
+    }
+}
+
 public protocol DSDatabaseViewQuery {
     static var viewQuery: String { get }
 }
@@ -86,5 +200,11 @@ public protocol Selfable {
 public extension Selfable {
     static var selfKey: String? {
         return nil
+    }
+}
+
+public extension DSViewField where Self: CodingKey {
+    var key: String {
+        return stringValue
     }
 }
