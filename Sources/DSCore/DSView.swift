ba5853db5b43d562dc5daf1d9067206ad2037940
Maher Santina <maher.santina90@gmail.com>
2020-05-02 16:44:18 +1000

    Added baseEntity for join

--- Sources/DSCore/DSView.swift
+++ Sources/DSCore/DSView.swift
@@ -38,29 +38,6 @@ public extension DSView {
     }
 }
 
-public extension DSJoinsRepresentableView {
-    static var viewQuery: String {
-        let select = ([mainEntity] + entities).map { (entity) -> [(String, DSViewField)] in
-            return entity.fields.map{ (entity.schema, $0) }
-        }
-        .flatMap{ $0 }
-        .map{ "`\($0.0)`.`\($0.1.key)` AS `\($0.0)_\($0.1.key)`" }
-        .joined(separator: ", ")
-
-        let joinClause = joins.map{ "\($0.joinType.query) `\($0.foreignEntity)` ON `\($0.foreignEntity)`.`\($0.foreignEntityKey)` = `\(mainEntity.schema)`.`\($0.entityKey)` " }.joined(separator: " ")
-
-        let final = """
-        SELECT
-        `\(mainEntity.schema)`.`\(mainEntity.fields.first?.key ?? "id")` AS `id`,
-        \(select)
-        FROM
-        `\(mainEntity.schema)`
-        \(joinClause)
-        """
-        return final
-    }
-}
-
 public extension DSViewField where Self: CodingKey {
     var key: String {
         return stringValue
@@ -85,14 +62,16 @@ public enum DSEntityJoin {
 }
 
 public struct Join {
-    public var foreignEntity: String
-    public var entityKey: String
-    public var foreignEntityKey: String
     public var joinType: DSEntityJoin
+    public var foreignEntity: String
+    public var baseEntity: String
+    public var baseEntityKey: String
+    public var foreignEntityKey: String
 
-    public init(foreignEntity: String, entityKey: String, foreignEntityKey: String, joinType: DSEntityJoin) {
+    public init(joinType: DSEntityJoin, baseEntity: String, baseEntityKey: String, foreignEntity: String,  foreignEntityKey: String) {
         self.foreignEntity = foreignEntity
-        self.entityKey = entityKey
+        self.baseEntity = baseEntity
+        self.baseEntityKey = baseEntityKey
         self.foreignEntityKey = foreignEntityKey
         self.joinType = joinType
     }
@@ -102,4 +81,27 @@ public protocol DSJoinsRepresentableView: DSView {
     static var mainEntity: ViewInformation { get }
     static var entities: [ViewInformation] { get }
     static var joins: [Join] { get }
+}
+
+public extension DSJoinsRepresentableView {
+    static var viewQuery: String {
+        let select = ([mainEntity] + entities).map { (entity) -> [(String, DSViewField)] in
+            return entity.fields.map{ (entity.schema, $0) }
+        }
+        .flatMap{ $0 }
+        .map{ "`\($0.0)`.`\($0.1.key)` AS `\($0.0)_\($0.1.key)`" }
+        .joined(separator: ", ")
+
+        let joinClause = joins.map{ "\($0.joinType.query) `\($0.foreignEntity)` ON `\($0.foreignEntity)`.`\($0.foreignEntityKey)` = `\($0.baseEntity)`.`\($0.baseEntityKey)` " }.joined(separator: " ")
+
+        let final = """
+        SELECT
+        `\(mainEntity.schema)`.`\(mainEntity.fields.first?.key ?? "id")` AS `id`,
+        \(select)
+        FROM
+        `\(mainEntity.schema)`
+        \(joinClause)
+        """
+        return final
+    }
 }
