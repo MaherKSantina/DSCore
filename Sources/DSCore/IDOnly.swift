5224566b4e26004df73699a9f62fe33cdb0add54
Maher Santina <maher.santina90@gmail.com>
2020-05-03 15:06:37 +1000

    Fixed stuff

--- Sources/DSCore/IDOnly.swift
+++ Sources/DSCore/IDOnly.swift
@@ -7,10 +7,28 @@
 
 import Vapor
 
-public final class IDOnly: Content {
+public final class IDOnlyData: Content {
     public var id: Int
 
     public init(id: Int) {
         self.id = id
+    }
+}
+
+public protocol IDOnly {
+    associatedtype IDValue: Codable, Hashable
+    var id: IDValue { get set }
+}
+
+public protocol EntityRelated: IDOnly {
+
+    associatedtype Entity: DSEntity
+
+    func entity(req: Request) -> EventLoopFuture<Entity?>
+}
+
+public extension EntityRelated where Self.IDValue == Entity.IDValue {
+    func entity(req: Request) -> EventLoopFuture<Entity?> {
+        return Entity.find(id, on: req.db)
     }
 }
