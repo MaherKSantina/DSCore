7983ae8f145c0e5c4c6cedac6f67fb44f31d698e
Maher Santina <maher.santina90@gmail.com>
2020-09-16 23:39:15 +1000

    Fixed package

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
             targets: ["DSCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.14.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.29.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0-rc.2"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
